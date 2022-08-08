19ee8a447e0c9930a630c41aa07b6894c14886e3
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2022-08-08 14:24:22 +0100

    Added some keyboard processing

--- main.js
+++ main.js
@@ -10,7 +10,31 @@ var gs={
   canvas:null,
   ctx:null,
   scale:1,
+
+  // Main character
+  keystate:0,
+  x:0, // x position
+  y:0, // y position
+  vs:0, // vertical speed
+  hs:0, // horizontal speed
+  jump:false, // jumping
+  fall:false, // falling
+  dir:0, //direction (-1=left, 0=none, 1=right)
+
+  debug:false
 };
+
+// Clear keyboard input state
+function clearinputstate()
+{
+  gs.keystate=0;
+}
+
+// Check if an input is set in keyboard input state
+function ispressed(keybit)
+{
+  return ((gs.keystate&keybit)!=0);
+}
 
 // Handle resize events
 function playfieldsize()
@@ -43,7 +67,72 @@ function playfieldsize()
 // Update the player key state
 function updatekeystate(e, dir)
 {
-  // TODO
+  switch (e.which)
+  {
+    case 37: // cursor left
+    case 65: // A
+    case 90: // Z
+      if (dir==1)
+        gs.keystate|=1;
+      else
+        gs.keystate&=~1;
+
+      e.preventDefault();
+      break;
+
+    case 38: // cursor up
+    case 87: // W
+    case 59: // semicolon
+      if (dir==1)
+        gs.keystate|=2;
+      else
+        gs.keystate&=~2;
+
+      e.preventDefault();
+      break;
+
+    case 39: // cursor right
+    case 68: // D
+    case 88: // X
+      if (dir==1)
+        gs.keystate|=4;
+      else
+        gs.keystate&=~4;
+
+      e.preventDefault();
+      break;
+
+    case 40: // cursor down
+    case 83: // S
+    case 190: // dot
+      if (dir==1)
+        gs.keystate|=8;
+      else
+        gs.keystate&=~8;
+
+      e.preventDefault();
+      break;
+
+    case 13: // enter
+    case 32: // space
+      if (dir==1)
+        gs.keystate|=16;
+      else
+        gs.keystate&=~16;
+
+      e.preventDefault();
+      break;
+
+    case 73: // I (for info/debug)
+      if (dir==1)
+        gs.debug=(!gs.debug);
+
+      e.preventDefault();
+      break;
+
+    default:
+      break;
+  }
 }
 
 // Entry point
