9881e4e48fd7b13dced0227169e2029cbf13f7ee
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2022-08-17 01:07:12 +0100

    Added platformer movement

--- main.js
+++ main.js
@@ -76,6 +76,16 @@ const tilesperrow=10;
 
 // Game state
 var gs={
+  // animation frame of reference
+  step:(1/60), // target step time @ 60 fps
+  acc:0, // accumulated time since last frame
+  lasttime:0, // time of last frame
+
+  // physics in pixels per frame @ 60fps
+  gravity:0.25,
+  terminalvelocity:10,
+  friction:1,
+
   // Canvas
   canvas:null, // Tiles
   ctx:null,
@@ -90,13 +100,23 @@ var gs={
   keystate:0,
   x:0, // x position
   y:0, // y position
+  px:0, // previous x position
+  py:0, // previous y position
   sx:0, // start x position (for current level)
   sy:0, // start y position (for current level)
   vs:0, // vertical speed
   hs:0, // horizontal speed
   jump:false, // jumping
   fall:false, // falling
+  duck:false, // ducking (for defence)
+  htime:0, // hurt timer following enemy collision
   dir:0, //direction (-1=left, 0=none, 1=right)
+  hsp:1, // max horizontal speed
+  vsp:1, // max vertical speed
+  speed:2, // walking speed
+  jumpspeed:5, // jumping speed
+  coyote:0, // coyote timer (time after leaving ground where you can still jump)
+  life:100, // remaining "life force" as percentage
 
   // Level attributes
   level:0, // Level number (0 based)
@@ -105,6 +125,10 @@ var gs={
 
   // Characters
   chars:[],
+  anim:8, // time until next character animation frame
+
+  // Game state
+  state:2, // state machine, 0=intro, 1=menu, 2=playing, 3=complete
 
   // Debug flag
   debug:false
@@ -234,7 +258,7 @@ function drawtile(tileid, x, y)
   //
   // gs.ctx.save();
   // gs.scale(-1, 1);
-  // gs.drawImage(gs.tilemap, (tileid*tilesize) % (tilesperrow*tilesize), Math.floor((tileid*tilesize) / (tilesperrow*tilesize))*tilesize, tilesize, tilesize, x, y, tilesize*-1, tilesize)
+  // gs.ctx.drawImage(gs.tilemap, (tileid*tilesize) % (tilesperrow*tilesize), Math.floor((tileid*tilesize) / (tilesperrow*tilesize))*tilesize, tilesize, tilesize, x, y, tilesize*-1, tilesize)
   // gs.ctx.restore();
 
   gs.ctx.drawImage(gs.tilemap, (tileid*tilesize) % (tilesperrow*tilesize), Math.floor((tileid*tilesize) / (tilesperrow*tilesize))*tilesize, tilesize, tilesize, x, y, tilesize, tilesize);
@@ -244,7 +268,7 @@ function drawtile(tileid, x, y)
 function drawsprite(sprite)
 {
   gs.sctx.drawImage(gs.tilemap, (sprite.id*tilesize) % (tilesperrow*tilesize), Math.floor((sprite.id*tilesize) / (tilesperrow*tilesize))*tilesize, tilesize, tilesize,
-    sprite.x, sprite.y, tilesize, tilesize);
+    Math.floor(sprite.x), Math.floor(sprite.y), tilesize, tilesize);
 }
 
 // Load level
@@ -327,6 +351,322 @@ function drawchars()
     drawsprite(gs.chars[id]);
 }
 
+// Check if player has left the map
+function offmapcheck()
+{
+  if ((gs.x<0) || (gs.y>levels[gs.level].height*tilesize))
+  {
+    gs.x=gs.sx;
+    gs.y=gs.sy;
+  }
+}
+
+// Check if one tile overlaps with another
+function overlap(ax, ay, bx, by)
+{
+  // Check horizontally
+  if ((ax<bx) && ((ax+tilesize))<=bx) return false; // a too far left of b
+  if ((ax>bx) && ((bx+tilesize))<=ax) return false; // a too far right of b
+
+  // Check vertically
+  if ((ay<by) && ((ay+tilesize))<=by) return false; // a too far above b
+  if ((ay>by) && ((by+tilesize))<=ay) return false; // a too far below b
+
+  return true;
+}
+
+function collide(px, py)
+{
+  // Look through all the tiles for a collision
+  for (var y=0; y<gs.height; y++)
+  {
+    for (var x=0; x<gs.width; x++)
+    {
+      var tile=parseInt(levels[gs.level].tiles[(y*gs.width)+x]||1, 10);
+
+      if ((tile-1)!=0)
+      {
+        if (overlap(px, py, x*tilesize, y*tilesize))
+          return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+// Check if player on the ground or falling
+function groundcheck()
+{
+  // Check for coyote time
+  if (gs.coyote>0)
+    gs.coyote--;
+
+  // Check we are on the ground
+  if (collide(gs.x, gs.y+1))
+  {
+    gs.vs=0;
+    gs.jump=false;
+    gs.fall=false;
+    gs.coyote=15;
+
+    // Check for jump pressed, when not ducking
+    if ((ispressed(16)) && (!gs.duck))
+    {
+      gs.jump=true;
+      gs.vs=-gs.jumpspeed;
+    }
+  }
+  else
+  {
+    // Check for jump pressed, when not ducking, and coyote time not expired
+    if ((ispressed(16)) && (!gs.duck) && (gs.jump==false) && (gs.coyote>0))
+    {
+      gs.jump=true;
+      gs.vs=-gs.jumpspeed;
+    }
+
+    // We're in the air, increase falling speed until we're at terminal velocity
+    if (gs.vs<gs.terminalvelocity)
+      gs.vs+=gs.gravity;
+
+    // Set falling flag when vertical speed is positive
+    if (gs.vs>0)
+      gs.fall=true;
+  }
+}
+
+// Process jumping
+function jumpcheck()
+{
+  // When jumping ..
+  if (gs.jump)
+  {
+    // Check if loosing altitude
+    if (gs.vs>=0)
+    {
+      gs.jump=false;
+      gs.fall=true;
+    }
+  }
+}
+
+// Move player by appropriate amount, up to a collision
+function collisioncheck()
+{
+  // Check for horizontal collisions
+  if (collide(gs.x+gs.hs, gs.y))
+  {
+    // A collision occured, so move the character until it hits
+    while (!collide(gs.x+(gs.hs>0?1:-1), gs.y))
+      gs.x+=(gs.hs>0?1:-1);
+
+    // Stop horizontal movement
+    gs.hs=0;
+  }
+  gs.x+=Math.floor(gs.hs);
+
+  // Check for vertical collisions
+  if (collide(gs.x, gs.y+gs.vs))
+  {
+    // A collision occured, so move the character until it hits
+    while (!collide(gs.x, gs.y+(gs.vs>0?1:-1)))
+      gs.y+=(gs.vs>0?1:-1);
+
+    // Stop vertical movement
+    gs.vs=0;
+  }
+  gs.y+=Math.floor(gs.vs);
+}
+
+// If no input detected, slow the player using friction
+function standcheck()
+{
+  // Check for ducking, or injured
+  if ((ispressed(8)) || (gs.htime>0))
+    gs.duck=true;
+  else
+    gs.duck=false;
+
+  // When no horizontal movement pressed, slow down by friction
+  if (((!ispressed(1)) && (!ispressed(4))) ||
+      ((ispressed(1)) && (ispressed(4))))
+  {
+    // Going left
+    if (gs.dir==-1)
+    {
+      if (gs.hs<0)
+      {
+        gs.hs+=gs.friction;
+      }
+      else
+      {
+        gs.hs=0;
+        gs.dir=0;
+      }
+    }
+
+    // Going right
+    if (gs.dir==1)
+    {
+      if (gs.hs>0)
+      {
+        gs.hs-=gs.friction;
+      }
+      else
+      {
+        gs.hs=0;
+        gs.dir=0;
+      }
+    }
+  }
+}
+
+// Move animation frame onwards
+function updateanimation()
+{
+  if (gs.anim==0)
+  {
+    for (var id=0; id<gs.chars.length; id++)
+    {
+      switch (gs.chars[id].id)
+      {
+        case 51:
+          gs.chars[id].id=52;
+          break;
+
+        case 52:
+          gs.chars[id].id=51;
+          break;
+
+        case 53:
+          gs.chars[id].id=54;
+          break;
+
+        case 54:
+          gs.chars[id].id=53;
+          break;
+
+        case 55:
+          gs.chars[id].id=56;
+          break;
+
+        case 56:
+          gs.chars[id].id=55;
+          break;
+
+        default:
+          break;
+      }
+    }
+
+    gs.anim=8;
+  }
+  else
+    gs.anim--;
+}
+
+// Update player movements
+function updatemovements()
+{
+  // Check if player has left the map
+  offmapcheck();
+
+  // Check if player on the ground or falling
+  groundcheck();
+
+  // Process jumping
+  jumpcheck();
+
+  // Move player by appropriate amount, up to a collision
+  collisioncheck();
+
+  // If no input detected, slow the player using friction
+  standcheck();
+
+  // When a horizontal movement key is pressed, adjust players horizontal speed and direction
+  if (gs.keystate!=0)
+  {
+    // Left key
+    if ((ispressed(1)) && (!ispressed(4)))
+    {
+      gs.hs=gs.htime==0?-gs.speed:-2;
+      gs.dir=-1;
+    }
+
+    // Right key
+    if ((ispressed(4)) && (!ispressed(1)))
+    {
+      gs.hs=gs.htime==0?gs.speed:2;
+      gs.dir=1;
+    }
+  }
+
+  // Decrease hurt timer
+  if (gs.htime>0) gs.htime--;
+
+  // Update any animation frames
+  updateanimation();
+}
+
+// Update function called once per frame
+function update()
+{
+  // Apply keystate/physics to player
+  updatemovements();
+
+  // Update other character movements / AI
+  // TODO
+
+  // Check for player/character collision
+  // TODO
+
+  // Check for player/collectable collision
+  // TODO
+}
+
+// Request animation frame callback
+function rafcallback(timestamp)
+{
+  // First time round, just save epoch
+  if (gs.lasttime>0)
+  {
+    // Determine accumulated time since last call
+    gs.acc+=((timestamp-gs.lasttime) / 1000);
+
+    // If it's more than 15 seconds since last call, reset
+    if ((gs.acc>gs.step) && ((gs.acc/gs.step)>(60*15)))
+      gs.acc=gs.step*2;
+
+    // Process "steps" since last call
+    while (gs.acc>gs.step)
+    {
+      update();
+      gs.acc-=gs.step;
+    }
+
+    // Clear the sprites canvas
+    gs.sctx.clearRect(0, 0, gs.scanvas.width, gs.scanvas.height);
+
+    // Draw the chars
+    drawchars();
+
+    // Draw the player
+    drawsprite({id:45, x:gs.x, y:gs.y});
+
+    // If the update took us out of play state then stop now
+    if (gs.state!=2)
+      return;
+  }
+
+  // Remember when we were last called
+  gs.lasttime=timestamp;
+
+  // Request we are called on the next frame, but only if still playing
+  if (gs.state==2)
+    window.requestAnimationFrame(rafcallback);
+}
+
 // Entry point
 function init()
 {
@@ -363,7 +703,7 @@ function init()
   playfieldsize();
 
   gs.tilemap=new Image;
-  gs.tilemap.onload=function() {loadlevel(0); drawlevel(); drawchars();};
+  gs.tilemap.onload=function() {loadlevel(0); drawlevel(); window.requestAnimationFrame(rafcallback);};
   gs.tilemap.src=tilemap;
 }
 
