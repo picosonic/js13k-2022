7bbc648baf285639132531207674f311a4b47748
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2022-09-10 23:26:12 +0100

    Fix for closure not recognising monetization

--- monetization.js
+++ monetization.js
@@ -5,14 +5,14 @@ var monetized=false;
 function setupmonetization()
 {
   // Check if browser supports it
-  if (document.monetization)
+  if (document['monetization'])
   {
     // Check for monetization being started
-    if (document.monetization.state==='started')
+    if (document['monetization'].state==='started')
       monetized=true;
     
     // Set up event listeners
-    document.monetization.addEventListener('monetizationstart', function() { monetized=true; });
-    document.monetization.addEventListener('monetizationstop', function() { monetized=false; });
+    document['monetization'].addEventListener('monetizationstart', function() { monetized=true; });
+    document['monetization'].addEventListener('monetizationstop', function() { monetized=false; });
   }
 }
